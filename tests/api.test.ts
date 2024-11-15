67e1f7700d87927168895c8458f27ff0d5f2365b
Fabio Fortini <fabio.fortini3@gmail.com>
2024-11-15 12:02:23 +0100

    start the implementation of execute use case for create post

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -69,3 +69,49 @@ describe("Create Post API", () => {
         expect(result.status).toEqual(400)
     })
 })
+
+class PostOfDomain {
+}
+
+interface Repository {
+    save(post: PostOfDomain): Post
+}
+
+class CreatePost implements CreatePostUseCase {
+    private _repository: Repository;
+
+    constructor(repository: Repository) {
+        this._repository = repository;
+    }
+
+    execute(userId: string, content: string): Post | CreatePostError {
+        return this._repository.save({
+            userId: userId,
+            content: content
+        })
+    }
+}
+
+describe("CreatePostUseCase implementation", () => {
+    it("returns the succesfully created post", () => {
+        const mockRepository = mock<Repository>()
+        mockRepository.save.mockReturnValue({
+            "postId": "saved-createdPost-id",
+            "userId": "1234",
+            "text": "This is a post",
+            "dateTime": "2018-01-10T11:30:00Z"
+        })
+        const createPostUseCase: CreatePostUseCase = new CreatePost(mockRepository);
+        const userId = "1234";
+        const postContent = "This is a post";
+
+        const result = createPostUseCase.execute(userId, postContent);
+
+        expect(result).toEqual({
+            "postId": "saved-createdPost-id",
+            "userId": "1234",
+            "text": "This is a post",
+            "dateTime": "2018-01-10T11:30:00Z"
+        })
+    })
+})
