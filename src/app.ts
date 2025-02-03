58b1e15579741d6d671ceef4c6c4ec1dd44ba783
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-02-03 15:38:11 +0100

    move up route dependency to usecase

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,18 @@ import {FastifyPluginAsync} from 'fastify';
 import {usersRoutes} from "./routes/users-routes";
 import {rootRoute} from "./routes/root-route";
 import sensible from "@fastify/sensible";
+import { RegisterUserUseCase } from './register-user-use-case';
+import { InMemoryUserRepository } from './in-memory-user-repository';
 
 export const app: FastifyPluginAsync = async (fastify): Promise<void> => {
+  const userRepository = new InMemoryUserRepository()
+  const registerUserUseCase = new RegisterUserUseCase(userRepository)
+
   // plugins
   void fastify.register(sensible)
   // routes
   void fastify.register(rootRoute)
-  void fastify.register(usersRoutes, { })
+  void fastify.register(usersRoutes, { registerUserUseCase })
 };
 
 export default app;
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,22 +1,20 @@
 import {FastifyPluginAsync, FastifyReply, FastifyRequest} from "fastify"
 import {RegisterUserRequest} from "../register-user-request";
 import {User} from "../user";
-import {InMemoryUserRepository} from "../in-memory-user-repository";
 import {RegisterUserUseCase} from "../register-user-use-case";
 import {FastifyInstance} from "fastify/types/instance";
 
-type UsersDependencies = {}
+type UsersDependencies = { registerUserUseCase: RegisterUserUseCase }
 type FastifyRegisterUserRequest = FastifyRequest<{ Body: RegisterUserRequest }>
 
 export const usersRoutes: FastifyPluginAsync<UsersDependencies> = async (fastify: FastifyInstance, deps) => {
-  const registerUserUseCase = new RegisterUserUseCase(new InMemoryUserRepository())
 
   fastify.get('/users', async function (request: FastifyRequest, reply: FastifyReply) {
     return []
   })
 
   fastify.post('/users', async function (request: FastifyRegisterUserRequest, reply: FastifyReply) {
-    const createdUser: User | undefined = registerUserUseCase.execute(request.body)
+    const createdUser: User | undefined = deps.registerUserUseCase.execute(request.body)
     if (!createdUser) {
       reply.status(400).send("Username already in use")
     }
