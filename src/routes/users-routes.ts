a3b99f16c62eddfa035765bde4c90d9bfcfc4096
Fabio Fortini <fabio.fortini3@gmail.com>
2025-02-03 14:40:24 +0100

    add type on register user request in users routes

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,13 +1,16 @@
 import {FastifyPluginAsync, FastifyReply, FastifyRequest} from "fastify"
+import {RegisterUserRequest} from "../register-user-request";
 
 type UsersDependencies = { }
+type FastifyRegisterUserRequest = FastifyRequest<{Body: RegisterUserRequest}>
+
 export const usersRoutes: FastifyPluginAsync<UsersDependencies> = async (fastify, deps) => {
 
   fastify.get('/users', async function (request, reply) {
     return []
   })
 
-  fastify.post('/users', async function (request: FastifyRequest, reply: FastifyReply) {
+  fastify.post('/users', async function (request: FastifyRegisterUserRequest, reply: FastifyReply) {
     return {
       id: "1",
       username: "pino",
