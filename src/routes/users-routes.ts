79705d8cf0cc8a17f3197804672277752344d02f
Fabio Fortini <fabio.fortini3@gmail.com>
2025-02-03 15:11:04 +0100

    implement from request handler to in memory repository of user registration

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,21 +1,66 @@
 import {FastifyPluginAsync, FastifyReply, FastifyRequest} from "fastify"
 import {RegisterUserRequest} from "../register-user-request";
+import {randomUUID} from "node:crypto";
 
-type UsersDependencies = { }
-type FastifyRegisterUserRequest = FastifyRequest<{Body: RegisterUserRequest}>
+type UsersDependencies = {}
+type FastifyRegisterUserRequest = FastifyRequest<{ Body: RegisterUserRequest }>
+
+interface UserRepository {
+  createUser(username: string, password: string, about: string): User | undefined
+}
+
+class RegisterUserUseCase {
+  private repository: UserRepository;
+
+  constructor(repository: UserRepository) {
+    this.repository = repository;
+  }
+
+  execute(user: RegisterUserRequest) {
+    return this.repository.createUser(user.username, user.password, user.about)
+  }
+}
+
+class InMemoryUserRepository implements UserRepository {
+  private users: Record<string, User> = {}
+  createUser(username: string, password: string, about: string): User | undefined {
+    if(this.users[username]) {
+      return undefined
+    }
+
+    this.users[username] = {
+      id: randomUUID(),
+      username,
+      about
+    }
+    return this.users[username];
+  }
+}
+
+interface User {
+  id: string;
+  username: string;
+  about: string;
+}
 
 export const usersRoutes: FastifyPluginAsync<UsersDependencies> = async (fastify, deps) => {
+  const registerUserUseCase = new RegisterUserUseCase(new InMemoryUserRepository())
 
   fastify.get('/users', async function (request, reply) {
     return []
   })
 
   fastify.post('/users', async function (request: FastifyRegisterUserRequest, reply: FastifyReply) {
+
+    const createdUser: User | undefined = registerUserUseCase.execute(request.body)
+
+    if (!createdUser) {
+      reply.status(400).send("Username already in use")
+    }
     return {
       id: "1",
       username: "pino",
       about: "ciao morris"
     }
   })
-
 }
--- test/routes/users-route.test.ts
+++ test/routes/users-route.test.ts
@@ -5,31 +5,44 @@ import {RegisterUserRequest} from "../../src/register-user-request";
 
 
 describe('users route', () => {
-  // registra un utente che non esiste -> ritorna l'utente con id
-  // registra un utente che esiste -> ritorna 400 Username already in use
-  test("register a new user", async (t) => {
-    const app = await runApp(t)
-    const request: RegisterUserRequest = {
-      username: "pino",
-      password: "dei palazzi",
-      about: "ciao morris"
-    }
+    test("register a new user", async (t) => {
+        const app = await runApp(t)
+        const request: RegisterUserRequest = {
+            username: "pino",
+            password: "dei palazzi",
+            about: "ciao morris"
+        }
 
-    const res = await app.inject({ method: 'POST', url: '/users',  body: request })
+        const res = await app.inject({method: 'POST', url: '/users', body: request})
 
-    assert.deepStrictEqual(JSON.parse(res.payload), {
-      id: "1",
-      username: "pino",
-      about: "ciao morris"
+        assert.deepStrictEqual(JSON.parse(res.payload), {
+            id: "1",
+            username: "pino",
+            about: "ciao morris"
+        })
     })
-  })
 
-  test('no users found', async (t) => {
-    const app = await runApp(t)
+    test("register an already existing user", async (t) => {
+        const app = await runApp(t)
+        const request: RegisterUserRequest = {
+            username: "pino",
+            password: "dei palazzi",
+            about: "ciao morris"
+        }
+        await app.inject({method: 'POST', url: '/users', body: request})
 
-    const res = await app.inject({ method: 'GET', url: '/users' })
+        const res = await app.inject({method: 'POST', url: '/users', body: request})
 
-    assert.equal(res.payload, '[]')
-  })
+        assert.equal(res.statusCode, 400)
+        assert.equal(res.payload, "Username already in use")
+    })
+
+    test('no users found', async (t) => {
+        const app = await runApp(t)
+
+        const res = await app.inject({method: 'GET', url: '/users'})
+
+        assert.equal(res.payload, '[]')
+    })
 })
 
