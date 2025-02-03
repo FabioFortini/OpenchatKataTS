6d2d9686d100700f1dd86bdc82fd1ade8e2da6c0
Fabio Fortini <fabio.fortini3@gmail.com>
2025-02-03 14:33:54 +0100

    add acceptance test of a registration of a user

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,10 +1,18 @@
-import { FastifyPluginAsync } from "fastify"
+import {FastifyPluginAsync, FastifyReply, FastifyRequest} from "fastify"
 
 type UsersDependencies = { }
 export const usersRoutes: FastifyPluginAsync<UsersDependencies> = async (fastify, deps) => {
 
   fastify.get('/users', async function (request, reply) {
     return []
+  })
+
+  fastify.post('/users', async function (request: FastifyRequest, reply: FastifyReply) {
+    return {
+      id: "1",
+      username: "pino",
+      about: "ciao morris"
+    }
   })
 
 }
--- test/routes/users-route.test.ts
+++ test/routes/users-route.test.ts
@@ -1,8 +1,29 @@
-import { test, describe } from 'node:test'
+import {describe, test} from 'node:test'
 import * as assert from 'node:assert'
-import { runApp } from '../helper'
+import {runApp} from '../helper'
+import {RegisterUserRequest} from "../../src/register-user-request";
+
 
 describe('users route', () => {
+  // registra un utente che non esiste -> ritorna l'utente con id
+  // registra un utente che esiste -> ritorna 400 Username already in use
+  test("register a new user", async (t) => {
+    const app = await runApp(t)
+    const request: RegisterUserRequest = {
+      username: "pino",
+      password: "dei palazzi",
+      about: "ciao morris"
+    }
+
+    const res = await app.inject({ method: 'POST', url: '/users',  body: request })
+
+    assert.deepStrictEqual(JSON.parse(res.payload), {
+      id: "1",
+      username: "pino",
+      about: "ciao morris"
+    })
+  })
+
   test('no users found', async (t) => {
     const app = await runApp(t)
 
@@ -11,3 +32,4 @@ describe('users route', () => {
     assert.equal(res.payload, '[]')
   })
 })
+
